e2f06b0262e88b1e7ae937195d7cd7a1ec25ce55
bfish713 <bjf2127@columbia.edu>
2013-11-05 20:47:53 -0800

    Create sched_setlimit.c

---
+++ arch/arm/kernel/sched_setlimit.c
@@ -0,0 +1,24 @@
+#include <linux/export.h>
+#include <linux/errno.h>
+#include <linux/sched.h>
+#include <linux/mm.h>
+#include <linux/sem.h>
+#include <linux/msg.h>
+#include <linux/shm.h>
+#include <linux/stat.h>
+#include <linux/syscalls.h>
+#include <linux/mman.h>
+#include <linux/fs.h>
+#include <linux/file.h>
+#include <linux/ipc.h>
+#include <linux/uaccess.h>
+#include <linux/slab.h>
+#include <asm/current.h> 
+
+asmlinkage int sched_setlimit(pid_t pid, int limit)
+{
+    struct task_struct *p = find_taks_by_vpid(pid);
+    
+    p->intervalLimit = limit*1000; //adjust into nanoseconds
+
+}
